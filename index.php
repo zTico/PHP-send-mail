e8a92c7ca97023cfa35679b2401683524f48f949
Thiago <thiagodiaslima10@gmail.com>
2020-12-22 18:16:13 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+    	<title>Sist enviar e-mail</title>
+		<link rel="stylesheet" type="text/css" href="estilo.css">
+    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	</head>
+
+	<body>
+		<div id="meio">	
+			<div class="py-3 text-center">
+				<img class="d-block mx-auto mb-2" src="logoultimate.png" alt="" width="140" height="125">
+				<h2>Enviar e-mail</h2>
+				<p class="lead">Sistema de envio de e-mail</p>
+			</div>
+
+			 <?php if( isset($_GET['erro']) ){?>
+				<div class="alert alert-danger" role="alert">
+					Preencha todos os campos
+				</div>
+			<?php } ?>
+			
+      		<div class="row">
+      			<div class="col-md-12">
+					<div class="card-body font-weight-bold">
+						<form method="POST" action= "processa_envio.php">
+							<div class="form-group">
+								<label for="para">Para</label>
+								<input type="email" class="form-control" name="para" placeholder="cerice@anoreg.com">
+							</div>
+
+							<div class="form-group">
+								<label for="assunto">Assunto</label>
+								<input type="text" class="form-control" name="assunto" placeholder="Assundo do e-mail">
+							</div>
+
+							<div class="form-group">
+								<label for="mensagem">Mensagem</label>
+								<textarea class="form-control" name="mensagem"></textarea>
+							</div>
+
+							<button type="submit" class="btn btn-primary btn-lg">Enviar Mensagem</button>
+						</form>
+					</div>
+				</div>	
+      		</div>
+		 </div>
+	</body>
+</html>
---
+++ processa_envio.php
@@ -0,0 +1,154 @@
+<?php   
+    require_once "./bibliotecas/PHPMailer/Exception.php";
+    require_once "./bibliotecas/PHPMailer/OAuth.php";
+    require_once "./bibliotecas/PHPMailer/PHPMailer.php";
+    require_once "./bibliotecas/PHPMailer/POP3.php";
+    require_once "./bibliotecas/PHPMailer/SMTP.php";
+    
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    class Mensagem {
+        private $para = null;
+        private $assunto = null;
+        private $mensagem = null;
+
+        public function __get($atributo){
+           return $this->$atributo;
+        }
+        public function __set($atributo, $valor){
+            $this->$atributo = $valor;
+        }
+        public function mensagemValida(){
+
+            if(!empty(strlen($this->para)) && !empty(strlen($this->assunto)) && !empty(strlen($this->mensagem))) {
+                return true;
+            } 
+            else {
+                return false;
+            }
+        }
+    }
+
+    $mensagem = new Mensagem();
+    $mensagem->__set('para', $_POST['para']);
+    $mensagem->__set('assunto', $_POST['assunto']);
+    $mensagem->__set('mensagem', $_POST['mensagem']);
+    echo $mensagem->mensagemValida();
+   
+    echo '<pre>';  var_dump($mensagem);
+    
+    if(!$mensagem->mensagemValida()){
+        header('Location:index.php?erro=Mensagem_Não_Enviada');
+    } 
+
+    $mail = new PHPMailer(true);
+    try {
+        //Server settings
+        $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'cericeteste@gmail.com';                 // SMTP username
+        $mail->Password = 'cerice12345';                           // SMTP password
+        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;                                    // TCP port to connect to
+
+        //Recipients
+        $mail->setFrom('cericeteste@gmail.com');
+        $mail->addAddress($mensagem->__get('para'));     // Add a recipient          // Name is optional
+        //$mail->addReplyTo('cericeteste@gmail.com', 'Information');
+        //$mail->addCC('cc@example.com');
+        //$mail->addBCC('bcc@example.com');
+
+        //Attachments
+        //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+        //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+        //Content
+        $mail->isHTML(true);                                   // Set email format to HTML
+        $mail->Subject = $mensagem->__get('assunto');
+        $mail->Body = '
+        <html>
+            <head>
+                <style type="text/css">
+                    
+                    .base{
+                        width: 400px;
+                        font-weight: bold;
+                        background: #FF6347;
+                        margin: auto;
+                    }
+                    .base h2{
+                        color: white;
+                        font-size: 30;
+                        text-align: center;
+                    } .base img{
+                        display: block;
+                        margin-left: auto;
+                        margin-right: auto;
+                        width: 50%;
+                    }
+                    .verificar h2{
+                        font-size: 25;
+                    }
+                    .meio {
+                        text-align: center;
+                        font-size: 18;
+                    }
+                    .meio2{
+                        padding-top: 3px;
+                        padding-bottom: 5px;
+                    }
+            
+                </style>
+            </head>
+            <body>
+            
+            <div class="base">
+                <h2>HoHoHoHoHo</h2> 
+                <img src="https://imagensemoldes.com.br/wp-content/uploads/Papai-Noel-1200x900.png" width="250px">
+                <div class="verificar">
+                    <h2>Verifiquei que seu assunto é: <br> '.$mensagem->__get('assunto').'</h2>
+                    <h2>E sua mensagem é: <br> '.$mensagem->__get('mensagem').'</h2>
+                </div>	
+                    <div class="meio">
+                        Logo iremos dar um retorno. <br>	
+                        <div class="meio2">Boas festar. Hohoho</div>
+                    </div>
+            
+            </div>
+            </body>
+        </html>';
+       // $mail->AltBody = 'Mensagem al email:';
+
+        $mail->send();
+       
+            if($mail){
+                function redireciona($link){
+                    if ($link==-1){
+                    echo" <script>history.go(-1);</script>";
+                    }else{
+                    echo "<script>alert('E-mail enviado com sucesso');</script>" ;  
+                    echo" <script>document.location.href='$link'</script>";
+                    }
+                };
+           
+               $link = 'index.php';
+               redireciona($link); 
+            }
+
+    } catch (Exception $e) {
+        echo 'Message could not be sent.';
+        echo 'Mailer Error: Erro ao execultar ';
+    }
+
+    
+
+
+   
+    
+
+
+
+ 
